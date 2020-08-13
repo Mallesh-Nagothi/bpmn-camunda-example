d4d2289843fc411ec89b217d93d1b397f82386b1
Mallesh Nagothi <malleswara.nd@gmail.com>
2020-08-13 16:35:13 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+import Modeler from "bpmn-js/lib/Modeler";
+import propertiesPanelModule from "bpmn-js-properties-panel";
+import propertiesProviderModule from "bpmn-js-properties-panel/lib/provider/camunda";
+
+import "bpmn-js/dist/assets/diagram-js.css";
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
+import "bpmn-js-properties-panel/dist/assets/bpmn-js-properties-panel.css";
+
+import * as camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda.json';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'bpmn-camunda-example';
+export class AppComponent implements OnInit {
+  title = 'Workflow Modeler';
+  modeler: Modeler;
+
+  @ViewChild('canvas')
+  private canvesRef: ElementRef;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.modeler = new Modeler({
+      container: '#canvas',
+      width: '100%',
+      height: '600px',
+      propertiesPanel: {
+        parent: '#properties'
+      },
+      additionalModules: [
+        propertiesPanelModule,
+        propertiesProviderModule
+      ],
+      moddleExtensions: {
+        camunda: camundaModdleDescriptor
+      }
+    });
+    this.load();
+  }
+
+  load(): void {
+    this.getExample().subscribe(data => {
+      this.modeler.importXML(data, value => this.handleError(value));
+    });
+  }
+
+  handleError(err: any) {
+    if (err) {
+      console.warn('Ups, error: ', err);
+    }
+  }
+
+  public getExample(): Observable<string> {
+    const url = '/assets/bpmn/initial.bpmn'; // local
+    return this.http.get(url, {responseType: 'text'});
+  }
+
 }
